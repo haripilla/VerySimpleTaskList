58c91adf84705f1a29e020784f4265ed1b3a0a91
Hari Pilla <Hari.Pilla@bcbsnc.com>
2017-04-19 10:26:36 -0400

    First VS Program V 1.0 - Added Welcome msg

--- VerySimpleTaskList/Program.cs
+++ VerySimpleTaskList/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace VerySimpleTaskList
+﻿using System;
+namespace VerySimpleTaskList
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to First VS program 1.0");
             TaskManager manager = new TaskManager();
             manager.Run();
         }
